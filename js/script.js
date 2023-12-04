bd22e87a720a3f68a2cb490cde9ca1bfa5816481
GiordanoDiego <diegomgiordano96@gmail.com>
2023-12-04 17:37:21 +0100

    added control km and finished

--- js/script.js
+++ js/script.js
@@ -14,45 +14,55 @@ let infoTicket = document.getElementById('infoTicket');
 let optionAge;
 //Setto il mio bottone
 myButton.addEventListener('click', function(){
+    
+
     km = document.getElementById('km');
     kmValue = parseInt(km.value);
-    document.getElementById('kmInput').innerHTML = kmValue;
     
-    userName = document.getElementById('userName');
-    userNameValue = userName.value;
-    
-    userLastName = document.getElementById('userLastName');
-    userLastNameValue = userLastName.value;
-
-    document.getElementById('userNameInput').innerHTML = userNameValue + " " + userLastNameValue;
-    
-    optionAge = document.getElementById('optionAge');
-    optionAge = optionAge.value;
-    document.getElementById('ageUserInput').innerHTML = optionAge;
-    console.log('option', optionAge, typeof optionAge);
-
-    //applico scontistica in base all'età
-    if (optionAge == 'minorenne'){
-        //sconto minori
-        console.log('Sconto minori');
-        priceTicket = kmValue * priceKm * discountMinor;
-        document.getElementById('price').innerHTML = priceTicket.toFixed(2) + '€ (scontato del 20%, sconto minori)';
-        console.log('prezzo', priceTicket.toFixed(2));
-    }
-    else if (optionAge == 'over65'){
-        //sconto over
-        console.log('Sconto over');
-        priceTicket = kmValue * priceKm * discountOver65;
-        document.getElementById('price').innerHTML = priceTicket.toFixed(2) + '€ (scontato del 40%, sconto over 65)';
-        console.log('prezzo', priceTicket.toFixed(2));
+    if(isNaN(kmValue)){
+        alert('Per favore inserisci i km da percorrere in numeri!')
     }
     else{
-        //niente sconto 
-        console.log('niente sconto');
-        priceTicket = kmValue * priceKm;
-        document.getElementById('price').innerHTML = priceTicket.toFixed(2) + '€';
-    }
 
+        document.getElementById('kmInput').innerHTML = kmValue;
+    
+        userName = document.getElementById('userName');
+        userNameValue = userName.value;
+        
+        userLastName = document.getElementById('userLastName');
+        userLastNameValue = userLastName.value;
+
+        document.getElementById('userNameInput').innerHTML = userNameValue + " " + userLastNameValue;
+        
+        optionAge = document.getElementById('optionAge');
+        optionAge = optionAge.value;
+        document.getElementById('ageUserInput').innerHTML = optionAge;
+        console.log('option', optionAge, typeof optionAge);
+
+        //applico scontistica in base all'età
+        if (optionAge == 'minorenne'){
+            //sconto minori
+            console.log('Sconto minori');
+            priceTicket = kmValue * priceKm * discountMinor;
+            document.getElementById('price').innerHTML = priceTicket.toFixed(2) + '€ (scontato del 20%, sconto minori)';
+            console.log('prezzo', priceTicket.toFixed(2));
+        }
+        else if (optionAge == 'over65'){
+            //sconto over
+            console.log('Sconto over');
+            priceTicket = kmValue * priceKm * discountOver65;
+            document.getElementById('price').innerHTML = priceTicket.toFixed(2) + '€ (scontato del 40%, sconto over 65)';
+            console.log('prezzo', priceTicket.toFixed(2));
+        }
+        else{
+            //niente sconto 
+            console.log('niente sconto');
+            priceTicket = kmValue * priceKm;
+            document.getElementById('price').innerHTML = priceTicket.toFixed(2) + '€';
+        }
+
+    }
+    
     //azzero il compilamento dell'utente
 
     km.value = '';
